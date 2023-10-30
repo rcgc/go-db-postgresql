59e7fdb26e1d1145720fc6245b3c31e30f36a1fd
rcgc <roberto140298@gmail.com>
2023-10-30 15:37:00 -0600

    migrations completed

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"log"
+
+	"github.com/rcgc/go-db-postgresql/pkg/invoiceitem"
 	"github.com/rcgc/go-db-postgresql/storage"
 )
 
 func main() {
 	storage.NewPostgresDB()
-	storage.NewPostgresDB()
-	storage.NewPostgresDB()
+
+	/*
+	storageProduct := storage.NewPsqlProduct(storage.Pool())
+	serviceProduct := product.NewService(storageProduct)
+	
+	if err := serviceProduct.Migrate(); err != nil {
+		log.Fatalf("product.Migrate: %v", err)
+	}
+	*/
+
+	/*
+	storageInvoiceHeader := storage.NewPsqlInvoiceHeader(storage.Pool())
+	serviceInvoiceHeader := invoiceheader.NewService(storageInvoiceHeader)
+
+	if err :=  serviceInvoiceHeader.Migrate(); err != nil {
+		log.Fatalf("invoiceHeader.Migrate: %v", err)
+	}
+	*/
+
+	storageInvoiceItem := storage.NewPsqlInvoiceItem(storage.Pool())
+	serviceInvoiceItem := invoiceitem.NewService(storageInvoiceItem)
+
+	if err := serviceInvoiceItem.Migrate(); err != nil {
+		log.Fatalf("invoiceItem.Migrate: %v", err)
+	}
 }
